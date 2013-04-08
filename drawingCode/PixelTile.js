ace831fd196328877f244c87a4baa07702491a81
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2013-04-07 20:56:40 -0400

    Separated code in different files. It is messy.

---
+++ drawingCode/PixelTile.js
@@ -0,0 +1,48 @@
+//  PixelTile class ///////////////////////////////////////////
+
+function PixelTile(w,h){
+
+	this.width=w;
+	this.height=h;
+
+	//this.webcolorData16=d16||(new Uint16Array(new ArrayBuffer(w*h*2)))
+	
+	this.image=document.createElement("canvas");
+	this.image.width=w;
+	this.image.height=h;
+	this.ctx=this.image.getContext("2d");
+	this.imageData=this.ctx.createImageData(w,h);
+	this.imageData32=new Int32Array(this.imageData.data.buffer);
+	
+	
+	this.pixelPutter=this.ctx.createImageData(1,1);
+	this.pixelPutter32=new Int32Array(this.pixelPutter.data.buffer);
+	
+	
+	//this.updateImage();
+	
+	this.t=this;
+	
+}
+
+
+PixelTile.prototype.putSinglePixel=function(x,y,webcolor){
+
+	this.pixelPutter32[0]=webcolorToBufcolor(webcolor);
+	this.ctx.putImageData(this.pixelPutter,x,y);
+
+}
+
+PixelTile.prototype.selectColor=function(x,y){
+
+	var webcolor =0;
+
+	var pixel=this.ctx.getImageData(x,y,1,1)
+	
+	webcolor|=(pixel.data[0] >>> 4)<<8;
+	webcolor|=(pixel.data[1] >>> 4)<<4;
+	webcolor|=(pixel.data[2] >>> 4)<<0;
+	
+	return webcolor;
+}
+
---
+++ drawingCode/basicUI.js
@@ -0,0 +1,73 @@
+
+window.addEventListener("load",function(){
+
+//jQuery commands for ui
+$(".sideBar").css("width",250);
+$("#screenContainer").css("left",250).css("right",250);
+$("#pinkButton")
+		.click(function(){
+			currentColor=initialPink;
+			updateDisplayer();
+		});
+$("#blackButton")
+		.click(function(){
+			currentColor=0x122;
+			updateDisplayer();
+		});
+$("#blueButton")
+		.click(function(){
+			currentColor=0x5fa;
+			updateDisplayer();
+		});
+
+var colorDisplayer=$("#colorDisplayer");
+
+
+function updateDisplayer(){
+
+	colorDisplayer.css("background-color", colorUtils.webcolorToCsscolor(currentColor)    )
+
+}
+	updateDisplayer();
+	
+	
+
+//get all the canvases. 	
+	
+	
+var mainCanvases=$(".mainCanvas");
+
+
+
+// Code to resize the canvas when the screen is resized //////////////
+
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+
+
+		for(var i=0;i<mainCanvases.length;i++){
+				
+				mainCanvases[i].width=window.innerWidth-500;
+				mainCanvases[i].height=window.innerHeight;	
+					
+		}		
+		
+		window.drawStuff||window.drawStuff();
+}
+
+
+if(window.mainPartLoaded){
+resizeCanvas();
+//placing the tile in the center
+position.x=canvas.width*0.5-tile.width*0.5*position.s;
+position.y=canvas.height*0.5-tile.height*0.5*position.s;
+
+drawStuff();
+}
+
+window.resizeCanvas=resizeCanvas;
+window.updateDisplayer=updateDisplayer;
+window.basicUILoaded=true;
+},false);
