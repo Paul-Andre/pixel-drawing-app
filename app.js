b6752a9547a515c41c7687aa091ed7248206c738
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2013-04-20 22:24:56 -0400

    tested canvas in node

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express')
   , app = express()
   ,sys = require('sys')
   , httpProxy= require('http-proxy')
-  ,colorUtils= require('./colorUtils.js');
+  ,colorUtils= require('./colorUtils.js')
+  ,Canvas=require('canvas')
+  ,fs=require('fs');
 
 
 app.use(express.static(__dirname+'/public'));
@@ -13,3 +15,9 @@ var proxy = httpProxy.createServer(8080, "127.0.0.1");
 proxy.listen(8000);
 
 console.log(colorUtils.webcolorToCsscolor(0xfff));
+
+var canvas=new Canvas(512,512);
+var ctx=canvas.getContext("2d");
+ctx.fillRect(5,24,234,234);
+var buf =canvas.toBuffer();
+fs.writeFile('image.png', buf);
