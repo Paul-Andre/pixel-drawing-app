6cdfa9ce0ee6ccbfea98168cbe387f3b6977496e
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2013-04-20 22:11:14 -0400

    "noded" colorUtils.js and placed testEndianness inside of it.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
   , app = express()
   ,sys = require('sys')
   , httpProxy= require('http-proxy')
+  ,colorUtils= require('./colorUtils.js');
 
 
 app.use(express.static(__dirname+'/public'));
@@ -10,3 +11,5 @@ app.listen(8080, "127.0.0.1");
 
 var proxy = httpProxy.createServer(8080, "127.0.0.1");
 proxy.listen(8000);
+
+console.log(colorUtils.webcolorToCsscolor(0xfff));
